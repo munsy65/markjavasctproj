db906dd5a97ee893cea2dfe8c762fd204c8ec1ef
munsy65 <munsy65@hotmail.com>
2015-01-26 14:23:43 -0800

    Java Script project

---
+++ pset8/js/houses.js
@@ -0,0 +1,24 @@
+/**
+ * houses.js
+ *
+ * Computer Science 50
+ * Problem Set 8
+ *
+ * Houses' approximate coordinates.
+ */
+
+var HOUSES = 
+{
+    "Adams House": {lat: 42.37189, lng: -71.11639, img: 'img/houseimg/adams.jpg'},
+    "Cabot House": {lat: 42.381842, lng: -71.123857, img: 'img/houseimg/cabot.jpg'},
+    "Currier House": {lat: 42.381704, lng: -71.125773, img: 'img/houseimg/currier.jpg'},
+    "Dunster House": {lat: 42.368648, lng: -71.115929, img: 'img/houseimg/dunster.jpg'},
+    "Eliot House": {lat: 42.370355, lng: -71.121041, img: 'img/houseimg/eliot.jpg'},
+    "Kirkland House": {lat: 42.370699, lng: -71.120859, img: 'img/houseimg/kirkland.jpg'},
+    "Leverett House": {lat: 42.36993, lng: -71.117238, img: 'img/houseimg/leverett.jpg'},
+    "Lowell House": {lat: 42.370572, lng: -71.118343, img: 'img/houseimg/lowell.jpg'},
+    "Mather House": {lat: 42.368196, lng: -71.115376, img: 'img/houseimg/mather.jpg'},
+    "Pforzheimer House": {lat: 42.38229, lng: -71.124839, img: 'img/houseimg/pforzheimer.jpg'},
+    "Quincy House": {lat: 42.370741, lng: -71.11698, img: 'img/houseimg/quincy.jpg'},
+    "Winthrop House": {lat: 42.370188, lng: -71.11955, img: 'img/houseimg/winthrop.jpg'}
+};
---
+++ pset8/js/service.js
@@ -0,0 +1,464 @@
+/**
+ * service.js
+ *
+ * Computer Science 50
+ * Problem Set 8
+ *
+ * Implements a shuttle service.
+ */
+
+// default height
+var HEIGHT = 0.8;
+
+// default latitude
+var LATITUDE = 42.3745615030193;
+
+// default longitude
+var LONGITUDE = -71.11803936751632;
+
+// default heading
+var HEADING = 1.757197490907891;
+
+// default number of seats
+var SEATS = 10;
+
+// default velocity
+var VELOCITY = 50;
+
+// global reference to shuttle's marker on 2D map
+var bus = null;
+
+// global reference to 3D Earth
+var earth = null;
+
+// global reference to 2D map
+var map = null;
+
+// global reference to shuttle
+var shuttle = null;
+
+//Location for riders
+var riders = new Array();
+var player_score = 0;
+
+// load version 1 of the Google Earth API
+google.load("earth", "1");
+
+// load version 3 of the Google Maps API
+google.load("maps", "3", {other_params: "sensor=false"});
+
+// once the window has loaded
+$(window).load(function() {
+
+    // listen for keydown anywhere in body
+    $(document.body).keydown(function(event) {
+        return keystroke(event, true);
+    });
+
+    // listen for keyup anywhere in body
+    $(document.body).keyup(function(event) {
+        return keystroke(event, false);
+    });
+
+    // listen for click on Drop Off button
+    $("#dropoff").click(function(event) {
+        dropoff();
+    });
+
+    // listen for click on Pick Up button
+    $("#pickup").click(function(event) {
+        pickup();
+    });
+
+    // load application
+    load();
+});
+
+// unload application
+$(window).unload(function() {
+    unload();
+});
+
+// return house object for passenger                
+function getHouseObject(passenger)
+{
+	for (house in HOUSES)
+	{
+		if (passenger.house == house)
+		{
+			return HOUSES[house];
+		}
+	}
+}
+
+/**
+ * Renders seating chart.
+ */
+function chart()
+{
+    var html = "<ol start='0'>";
+    for (var i = 0; i < shuttle.seats.length; i++)
+    {
+        if (shuttle.seats[i] == null)
+        {
+            html += "<li id='empty'>Empty Seat</li>";
+        }
+        else
+        {
+            passenger = shuttle.seats[i];
+            house = getHouseObject(passenger);
+            html += "<li>" + passenger.name + " to " + passenger.house +"  <img class='iconimg' src='" + house.img + "''>"  + "</li>";
+        }
+    }
+    html += "</ol>";
+    $("#chart").html(html);
+}
+
+/**
+ * Drops up passengers if their stop is nearby.
+ */
+function dropoff()
+{
+   var hey = "No Drop off";
+   
+   for(var p in shuttle.seats)
+   {
+        if(shuttle.seats[p] != null)
+        {
+            drop_house = HOUSES[shuttle.seats[p].house];
+            distance = shuttle.distance(drop_house.lat, drop_house.lng);
+            if(distance < 30.0)
+            {
+                shuttle.seats[p] = null;
+                player_score = player_score +1;
+                hey = "Drop off sucessfull!"+"<div id='score'>"+" Score: " +player_score+"</div>";
+            }
+        }
+   }
+   chart();
+   $("#announcements").html(hey);
+}
+
+/**
+ * Called if Google Earth fails to load.
+ */
+function failureCB(errorCode) 
+{
+    // report error unless plugin simply isn't installed
+    if (errorCode != ERR_CREATE_PLUGIN)
+    {
+        alert(errorCode);
+    }
+}
+
+/**
+ * Handler for Earth's frameend event.
+ */
+function frameend() 
+{
+    shuttle.update();
+}
+
+/**
+ * Called once Google Earth has loaded.
+ */
+function initCB(instance) 
+{
+    // retain reference to GEPlugin instance
+    earth = instance;
+
+    // specify the speed at which the camera moves
+    earth.getOptions().setFlyToSpeed(100);
+
+    // show buildings
+    earth.getLayerRoot().enableLayerById(earth.LAYER_BUILDINGS, true);
+
+    // disable terrain (so that Earth is flat)
+    earth.getLayerRoot().enableLayerById(earth.LAYER_TERRAIN, false);
+
+    // prevent mouse navigation in the plugin
+    earth.getOptions().setMouseNavigationEnabled(false);
+
+    // instantiate shuttle
+    shuttle = new Shuttle({
+        heading: HEADING,
+        height: HEIGHT,
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        planet: earth,
+        seats: SEATS,
+        velocity: VELOCITY
+    });
+
+    // synchronize camera with Earth
+    google.earth.addEventListener(earth, "frameend", frameend);
+
+    // synchronize map with Earth
+    google.earth.addEventListener(earth.getView(), "viewchange", viewchange);
+
+    // update shuttle's camera
+    shuttle.updateCamera();
+
+    // show Earth
+    earth.getWindow().setVisibility(true);
+
+    // render seating chart
+    chart();
+
+    // populate Earth with passengers and houses
+    populate();
+}
+
+/**
+ * Handles keystrokes.
+ */
+function keystroke(event, state)
+{
+    // ensure we have event
+    if (!event)
+    {
+        event = window.event;
+    }
+    var hey = "Shuttle Lat: "+shuttle.position.latitude.toFixed(6) + " Long: "  +shuttle.position.longitude.toFixed(6)+ "<div id='score'> " +"Score: "+player_score+"</div>";
+    $("#announcements").html(hey);
+
+    // left arrow
+    if (event.keyCode == 37)
+    {
+        shuttle.states.turningLeftward = state;
+        return false;
+    }
+
+    // up arrow
+    else if (event.keyCode == 38)
+    {
+        shuttle.states.tiltingUpward = state;
+        return false;
+    }
+
+    // right arrow
+    else if (event.keyCode == 39)
+    {
+        shuttle.states.turningRightward = state;
+        return false;
+    }
+
+    // down arrow
+    else if (event.keyCode == 40)
+    {
+        shuttle.states.tiltingDownward = state;
+        return false;
+    }
+
+    // A, a
+    else if (event.keyCode == 65 || event.keyCode == 97)
+    {
+        shuttle.states.slidingLeftward = state;
+        return false;
+    }
+
+    // D, d
+    else if (event.keyCode == 68 || event.keyCode == 100)
+    {
+        shuttle.states.slidingRightward = state;
+        return false;
+    }
+  
+    // S, s
+    else if (event.keyCode == 83 || event.keyCode == 115)
+    {
+        shuttle.states.movingBackward = state;     
+        return false;
+    }
+
+    // W, w
+    else if (event.keyCode == 87 || event.keyCode == 119)
+    {
+        shuttle.states.movingForward = state;    
+        return false;
+    }
+  
+    return true;
+}
+
+/**
+ * Loads application.
+ */
+function load()
+{
+    // embed 2D map in DOM
+    var latlng = new google.maps.LatLng(LATITUDE, LONGITUDE);
+    map = new google.maps.Map($("#map").get(0), {
+        center: latlng,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        zoom: 17,
+        zoomControl: true
+    });
+
+    // prepare shuttle's icon for map
+    bus = new google.maps.Marker({
+        icon: "https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/bus.png",
+        map: map,
+        title: "you are here"
+    });
+
+    // embed 3D Earth in DOM
+    google.earth.createInstance("earth", initCB, failureCB);
+}
+
+//function for finding a  seat for rider
+function find_seat()
+{
+    for(var s in shuttle.seats)
+        {
+            if(shuttle.seats[s] == null)
+                {
+                    return s;
+                }
+         }
+         return -1;
+}                
+
+
+
+
+
+/**
+ * Picks up nearby passengers.
+ */
+function pickup()
+{
+    var hey = "No Pickup";
+    var openSeat = find_seat();
+    
+    if(openSeat != -1)
+    {
+        for( var x in riders)
+        { 
+            var distance = shuttle.distance(riders[x].lat, riders[x].lng);
+         
+         if(distance  < 15.0 && HOUSES[riders[x].house] != null && riders[x].placemark != null)
+         {
+         
+            hey = "Sucessful Pickup!";
+            shuttle.seats[openSeat] = riders[x];
+            earth.getFeatures().removeChild(riders[x].placemark);
+            riders[x].placemark = null;
+            riders[x].marker.setMap(null);
+            chart();
+            openSeat = find_seat();
+            if(openSeat == -1)
+            {
+                break;
+            }   
+            
+        }
+    }
+}
+    else
+    {
+        hey = "No Seats!";
+    }
+    
+    $("#announcements").html(hey);
+}    
+
+/**
+ * Populates Earth with passengers and houses.
+ */
+function populate()
+{
+    // mark houses
+    for (var house in HOUSES)
+    {
+        // plant house on map
+        new google.maps.Marker({
+            icon: "https://google-maps-icons.googlecode.com/files/home.png",
+            map: map,
+            position: new google.maps.LatLng(HOUSES[house].lat, HOUSES[house].lng),
+            title: house
+        });
+    }
+
+    // get current URL, sans any filename
+    var url = window.location.href.substring(0, (window.location.href.lastIndexOf("/")) + 1);
+
+    // scatter passengers
+    for (var i = 0; i < PASSENGERS.length; i++)
+    {
+        // pick a random building
+        var building = BUILDINGS[Math.floor(Math.random() * BUILDINGS.length)];
+
+        // prepare placemark
+        var placemark = earth.createPlacemark("");
+        placemark.setName(PASSENGERS[i].name + " to " + PASSENGERS[i].house);
+
+        // prepare icon
+        var icon = earth.createIcon("");
+        icon.setHref(url + "/img/" + PASSENGERS[i].username + ".jpg");
+
+        // prepare style
+        var style = earth.createStyle("");
+        style.getIconStyle().setIcon(icon);
+        style.getIconStyle().setScale(4.0);
+
+        // prepare stylemap
+        var styleMap = earth.createStyleMap("");
+        styleMap.setNormalStyle(style);
+        styleMap.setHighlightStyle(style);
+
+        // associate stylemap with placemark
+        placemark.setStyleSelector(styleMap);
+
+        // prepare point
+        var point = earth.createPoint("");
+        point.setAltitudeMode(earth.ALTITUDE_RELATIVE_TO_GROUND);
+        point.setLatitude(building.lat);
+        point.setLongitude(building.lng);
+        point.setAltitude(0.0);
+
+        // associate placemark with point
+        placemark.setGeometry(point);
+
+        // add placemark to Earth
+        earth.getFeatures().appendChild(placemark);
+
+        // add marker to map
+        var marker = new google.maps.Marker({
+            icon: "https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/man.png",
+            map: map,
+            position: new google.maps.LatLng(building.lat, building.lng),
+            title: PASSENGERS[i].name + " at " + building.name
+        });
+
+       riders[i] = PASSENGERS[i];
+       riders[i].lat = building.lat;
+       riders[i].lng = building.lng;
+       riders[i].name = PASSENGERS[i].name;
+       riders[i].placemark = placemark;
+       riders[i].house = PASSENGERS[i].house;
+       riders[i].marker = marker;
+    }
+}
+
+/**
+ * Handler for Earth's viewchange event.
+ */
+function viewchange() 
+{
+    // keep map centered on shuttle's marker
+    var latlng = new google.maps.LatLng(shuttle.position.latitude, shuttle.position.longitude);
+    map.setCenter(latlng);
+    bus.setPosition(latlng);
+}
+
+/**
+ * Unloads Earth.
+ */
+function unload()
+{
+    google.earth.removeEventListener(earth.getView(), "viewchange", viewchange);
+    google.earth.removeEventListener(earth, "frameend", frameend);
+}
